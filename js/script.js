2ef5b28e84e8d8187e92b6c196a0801075cff796
G2yoshy <74261855+G2yoshy@users.noreply.github.com>
2026-06-17 09:54:35 +0200

    audio?????

--- js/script.js
+++ js/script.js
@@ -12,93 +12,87 @@ function playStep(overlaySrc, newBg, nextState) {
     //  preparar overlay
     overlay.src = overlaySrc;
     overlay.load();
-    overlay.src = "videos/video1.mp4";
-    overlay.load();
 
-    overlay.muted = false; // 🔊 activar sonido
+    overlay.classList.add("show");
+    overlay.classList.remove("hidden");
 
-    overlay.play().catch(err => {
-        console.log("Autoplay bloqueado:", err);
-        overlay.classList.add("show");
-        overlay.classList.remove("hidden");
+    overlay.play();
 
+    overlay.onended = () => {
 
+        //  INICIO TRANSICIÓN APPLE
+        bgVideo.classList.add("transitioning");
+        overlay.classList.remove("show");
+        
+        
+        setTimeout(() => {
 
-        overlay.onended = () => {
+            //  cambiar fondo
+            bgVideo.src = newBg;
+            bgVideo.load();
+            bgVideo.play();
 
-            //  INICIO TRANSICIÓN APPLE
-            bgVideo.classList.add("transitioning");
-            overlay.classList.remove("show");
-
-
+            //  restaurar fondo
             setTimeout(() => {
+                bgVideo.classList.remove("transitioning");
+            }, 300);
 
-                //  cambiar fondo
-                bgVideo.src = newBg;
-                bgVideo.load();
-                bgVideo.play();
+            // ocultar overlay
+            overlay.classList.add("hidden");
 
-                //  restaurar fondo
-                setTimeout(() => {
-                    bgVideo.classList.remove("transitioning");
-                }, 300);
+            // guardar progreso
+            localStorage.setItem("progreso", nextState);
 
-                // ocultar overlay
-                overlay.classList.add("hidden");
-
-                // guardar progreso
-                localStorage.setItem("progreso", nextState);
-
-            }, 600);
-        };
-    }
+        }, 600);
+    };
+}
 // NFC START 
 if (nfc === "start") {
-        localStorage.setItem("progreso", "1");
-        progreso = 1;
+    localStorage.setItem("progreso", "1");
+    progreso = 1;
+}
+
+// NFC 1
+if (nfc === "video1") {
+
+    if (progreso >= 1) {
+        playStep(
+            //cambia vide1 y fondo2 por tus videos
+            "resources/clip1.mp4",
+            "resources/clip2.mp4",
+            2
+        );
+    } else {
+        alert("Primero activa el NFC inicial");
     }
+}
 
-    // NFC 1
-    if (nfc === "video1") {
+//NFC 2
+if (nfc === "video2") {
 
-        if (progreso >= 1) {
-            playStep(
-                //cambia vide1 y fondo2 por tus videos
-                "resources/clip1.mp4",
-                "resources/clip2.mp4",
-                2
-            );
-        } else {
-            alert("Primero activa el NFC inicial");
-        }
+    if (progreso >= 2) {
+        playStep(
+            //cambia vide2 y fondo3 por tus videos
+            "resources/video2.mp4",
+            "resources/fondo3.mp4",
+            3
+        );
+    } else {
+        alert("Acceso bloqueado");
     }
+}
 
-    //NFC 2
-    if (nfc === "video2") {
+//NFC 3
+if (nfc === "video3") {
 
-        if (progreso >= 2) {
-            playStep(
-                //cambia vide2 y fondo3 por tus videos
-                "resources/video2.mp4",
-                "resources/fondo3.mp4",
-                3
-            );
-        } else {
-            alert("Acceso bloqueado");
-        }
+    if (progreso >= 3) {
+        playStep(
+            //cambia vide1 y video2 por tus videos
+            "resources/video3.mp4",
+            "resources/fondo4.mp4",
+            3
+        );
+    } else {
+        alert("Acceso bloqueado");
     }
-
-    //NFC 3
-    if (nfc === "video3") {
-
-        if (progreso >= 3) {
-            playStep(
-                //cambia vide1 y video2 por tus videos
-                "resources/video3.mp4",
-                "resources/fondo4.mp4",
-                3
-            );
-        } else {
-            alert("Acceso bloqueado");
-        }
-    }
+}
